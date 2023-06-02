28a0501feb3281354c0f02cd9af59310a31da0c1
paul-tes <paulasca8462@gmail.com>
2023-06-02 13:53:53 +0300

    calculator functionality

--- src/components/keys.jsx
+++ src/components/keys.jsx
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import { React } from 'react';
+import PropTypes from 'prop-types';
 
-class Keys extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Keys = ({ tabCalculator }) => (
+  <>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp">AC</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp">+/-</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp">%</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp-s">÷</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">7</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">8</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">9</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp-s">x</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">4</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">5</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">6</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp-s">-</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">1</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">2</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">3</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp-s">+</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num span-2">0</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-num">.</button>
+    <button onClick={(e) => tabCalculator(e)} type="button" className="btn btn-opp-s">=</button>
+  </>
+);
 
-  render() {
-    return (
-      <>
-        <button type="button" className="btn btn-opp">AC</button>
-        <button type="button" className="btn btn-opp">+/-</button>
-        <button type="button" className="btn btn-opp">%</button>
-        <button type="button" className="btn btn-opp-s">÷</button>
-        <button type="button" className="btn btn-num">7</button>
-        <button type="button" className="btn btn-num">8</button>
-        <button type="button" className="btn btn-num">9</button>
-        <button type="button" className="btn btn-opp-s">x</button>
-        <button type="button" className="btn btn-num">4</button>
-        <button type="button" className="btn btn-num">5</button>
-        <button type="button" className="btn btn-num">6</button>
-        <button type="button" className="btn btn-opp-s">-</button>
-        <button type="button" className="btn btn-num">1</button>
-        <button type="button" className="btn btn-num">2</button>
-        <button type="button" className="btn btn-num">3</button>
-        <button type="button" className="btn btn-opp-s">+</button>
-        <button type="button" className="btn btn-num span-2">0</button>
-        <button type="button" className="btn btn-num">.</button>
-        <button type="button" className="btn btn-opp-s">=</button>
-      </>
-    );
-  }
-}
+Keys.propTypes = {
+  tabCalculator: PropTypes.func.isRequired,
+};
 
 export default Keys;
