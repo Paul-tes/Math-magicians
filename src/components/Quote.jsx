9d2c3819f11dc4324fa011e09cd9fef69f88c19e
paul-tes <paulasca8462@gmail.com>
2023-06-03 11:16:30 +0300

    Add style on quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -9,62 +9,11 @@ const Quote = () => {
   });
   const [status, setStatus] = useState(null);
   const catagory = ['age', 'alone', 'amazing', 'anger', 'architecture', 'art', 'attitude', 'beauty', 'best', 'birthday', 'business',
-    'car',
-    'change',
-    'communications',
-    'computers',
-    'cool',
-    'courage',
-    'dad',
-    'dating',
-    'death',
-    'design',
-    'dreams',
-    'education',
-    'environmental',
-    'equality',
-    'experience',
-    'failure',
-    'faith',
-    'family',
-    'famous',
-    'fear',
-    'fitness',
-    'food',
-    'forgiveness',
-    'freedom',
-    'friendship',
-    'funny',
-    'future',
-    'god',
-    'good',
-    'government',
-    'graduation',
-    'great',
-    'happiness',
-    'health',
-    'history',
-    'home',
-    'hope',
-    'humor',
-    'imagination',
-    'inspirational',
-    'intelligence',
-    'jealousy',
-    'knowledge',
-    'leadership',
-    'learning',
-    'legal',
-    'life',
-    'love',
-    'marriage',
-    'medical',
-    'men',
-    'mom',
-    'money',
-    'morning',
-    'movies',
-    'success'];
+    'car', 'change', 'communications', 'computers', 'cool', 'courage', 'dad', 'dating', 'death', 'design', 'dreams',
+    'education', 'environmental', 'equality', 'experience', 'failure', 'faith', 'family', 'famous', 'fear', 'fitness', 'food',
+    'forgiveness', 'freedom', 'friendship', 'funny', 'future', 'god', 'good', 'government', 'graduation', 'great', 'happiness',
+    'health', 'history', 'home', 'hope', 'humor', 'imagination', 'inspirational', 'intelligence', 'jealousy', 'knowledge',
+    'leadership', 'learning', 'legal', 'life', 'love', 'marriage', 'medical', 'men', 'mom', 'money', 'morning', 'movies', 'success'];
   const index = Math.floor(Math.random() * catagory.length + 1);
   const url = `https://api.api-ninjas.com/v1/quotes?category=${catagory[index]}`;
   const key = '1ZjiiTojrbnI2qvRhVgFQBfsw42MEyghy7v2xadD';
@@ -89,9 +38,10 @@ const Quote = () => {
     <div className="quote-container">
       <div className="quote">
         <p className="catagory-p">
-          Category:
           {' '}
+          <a href="qoute">&lt; </a>
           {data.category}
+          <a href="qoute"> &gt;</a>
         </p>
         <p className="q-p">
           {data.quote}
