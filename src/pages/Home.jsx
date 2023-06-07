4fb15ad56e22dd4f0c81f780d657f8c66244c6da
paul-tes <paulasca8462@gmail.com>
2023-06-07 17:38:46 +0300

    Home paragraph style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,28 @@
 import style from '../style/Home.module.css';
 
-const Home = () => (
-  <div className={style.container}>
-    <h2>Welcome to our page!</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, alias exercitationem! Est,
-      quisquam dolores fuga maxime fugiat exercitationem voluptate voluptatem odio labore magnam
-      aliquid magni incidunt maiores rerum at repellendus.
-    </p>
-    <br />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, alias exercitationem!
-      Est, quisquam dolores fuga maxime fugiat exercitationem voluptate voluptatem odio labore
-      magnam aliquid magni incidunt maiores rerum at repellendus.
-    </p>
-  </div>
-);
+const Home = () => {
+  const paragraphStyle = {
+    textAlign: 'center',
+    color: '#222',
+    fontWeight: '300',
+  };
+
+  return (
+    <div className={style.container}>
+      <h2>Welcome to our page!</h2>
+      <p style={paragraphStyle}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, alias exercitationem! Est,
+        quisquam dolores fuga maxime fugiat exercitationem voluptate voluptatem odio labore magnam
+        aliquid magni incidunt maiores rerum at repellendus.
+      </p>
+      <br />
+      <p style={paragraphStyle}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, alias exercitationem!
+        Est, quisquam dolores fuga maxime fugiat exercitationem voluptate voluptatem odio labore
+        magnam aliquid magni incidunt maiores rerum at repellendus.
+      </p>
+    </div>
+  );
+};
 
 export default Home;
